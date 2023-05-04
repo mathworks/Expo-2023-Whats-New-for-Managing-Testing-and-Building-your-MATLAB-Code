c2405d39b6eec804ca6a19a96c7e8c8c2a1db81d
Adamandios Sifounakis <asifouna@mathworks.com>
2023-05-04 18:00:12 -0400

    Added ability to detect hand type.

--- PokerHandsToolbox/PokerHand.m
+++ PokerHandsToolbox/PokerHand.m
@@ -2,6 +2,10 @@ classdef PokerHand < handle
     %POKERHAND Create and identify poker hands
     %   Provides an easy way to represent and compare poker hands
 
+    properties (SetAccess=private)
+        Type = "Empty";
+    end
+    
     properties
         Cards
     end
@@ -29,6 +33,74 @@ classdef PokerHand < handle
             else
                 obj.Cards = [];
             end
+
+            obj.determineHandType();
+        end
+    end
+
+    methods (Access = private)
+        function determineHandType(obj)
+            %DETERMINEHANDTYPE Determines hand type
+
+            % Exit early if "Empty"
+            if height(obj.Cards) == 0
+                obj.Type = "Empty";
+                return;
+            end
+
+            % Get card stats
+            maxCardValue = max(obj.Cards.Value);
+
+            [numOfCardsFound,valueOfCards] = groupcounts(obj.Cards.Value);
+            singleVals = valueOfCards(numOfCardsFound==1);
+            pairVals = valueOfCards(numOfCardsFound==2);
+            tripVals = valueOfCards(numOfCardsFound==3);
+            quadVals = valueOfCards(numOfCardsFound==4);
+
+            isStraightAceConsideredHigh = all(abs(diff(obj.Cards.Value)) == 1);
+            isStraightAceConsideredLow = all(ismember([2,3,4,5,14],obj.Cards.Value));
+            isStraight = isStraightAceConsideredHigh || isStraightAceConsideredLow;
+            if isStraightAceConsideredLow
+                maxCardValue = 5;
+            end
+
+            isFlush = all(obj.Cards.Suit(1) == obj.Cards.Suit);
+
+            % Determine hand type by characteristics
+            if isStraight && isFlush && maxCardValue == 14
+                obj.Type = "RoyalFlush";
+
+            elseif isStraight && isFlush
+                obj.Type = "StraightFlush";
+
+            elseif ~isempty(quadVals)
+                obj.Type = "FourOfAKind";
+
+            elseif ~isempty(tripVals) && ~isempty(pairVals)
+                obj.Type = "FullHouse";
+
+            elseif isFlush
+                obj.Type = "Flush";
+
+            elseif isStraight
+                obj.Type = "Straight";
+
+            elseif ~isempty(tripVals)
+                obj.Type = "ThreeOfAKind";
+
+            elseif numel(pairVals) == 2
+                obj.Type = "TwoPair";
+
+            elseif numel(pairVals) == 1
+                obj.Type = "Pair";
+
+            elseif numel(singleVals) == 5
+                obj.Type = "Single";
+
+            else
+                obj.Type = "Invalid";
+            end
+
         end
     end
 
