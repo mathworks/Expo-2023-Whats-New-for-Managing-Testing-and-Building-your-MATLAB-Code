082ac5903b9d1d41b4cdcf61860011da6b48b329
Adamandios Sifounakis <asifouna@mathworks.com>
2023-05-04 19:10:03 -0400

    Intentionally introduced issue in pokerHandStats for future code analysis demo.

--- Example_DependencyAnalyzer/pokerHandStats.m
+++ Example_DependencyAnalyzer/pokerHandStats.m
@@ -21,16 +21,16 @@ stats = pokerHandStatsTable;
 
 % Only return specific hand type stats on when requested
 if ~isempty(handType)
-    % ismember(stats.HandType,handType);
-    % stats = stats(ans,:);
+    ismember(stats.HandType,handType);
+    stats = stats(ans,:);
 
     % Using "ans" in your code can introduce unreliable behavior because
     % "ans" can be silently overwritten often. Replacing the rest of the
     % code in this "if" block with the code below will ensure more robust
     % code behavior.
     
-    ind = ismember(stats.HandType,handType);
-    stats = stats(ind,:);
+    % ind = ismember(stats.HandType,handType);
+    % stats = stats(ind,:);
 end
 
 end
